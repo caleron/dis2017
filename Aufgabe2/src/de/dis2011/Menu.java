a1fe9dd5ec3f67109a884f1f6070e75bd8e87376
caleron <paddy.buchhardt@arcor.de>
2017-04-18 23:22:09 +0200

    reformatted code and modified and renamed Makler class to EstateAgent

--- Aufgabe2/src/de/dis2011/Menu.java
+++ Aufgabe2/src/de/dis2011/Menu.java
@@ -10,14 +10,14 @@ import java.util.ArrayList;
  * Zuvor müssen mit addEntry Menüoptionen hinzugefügt werden. Mit
  * der Methode show() wird das Menü angezeigt und die mit der Option
  * angegebene Konstante zurückgeliefert.
- * 
+ * <p>
  * Beispiel:
  * Menu m = new Menu("Hauptmenü");
  * m.addEntry("Hart arbeiten", 0);
  * m.addEntry("Ausruhen", 1);
  * m.addEntry("Nach Hause gehen", 2);
  * int wahl = m.show();
- * 
+ * <p>
  * Angezeigt wird dann das Menü:
  * Hauptmenü:
  * [1] Hart arbeiten
@@ -26,60 +26,61 @@ import java.util.ArrayList;
  * --
  */
 public class Menu {
-	private String title;
-	private ArrayList<String> labels = new ArrayList<String>();
-	private ArrayList<Integer> returnValues = new ArrayList<Integer>();
-	
-	/**
-	 * Konstruktor.
-	 * @param title Titel des Menüs z.B. "Hauptmenü"
-	 */
-	public Menu(String title) {
-		super();
-		this.title = title;
-	}
-	
-	/**
-	 * Fügt einen Menüeintrag zum Menü hinzu
-	 * @param label Name des Eintrags
-	 * @param returnValue Konstante, die bei Wahl dieses Eintrags zurückgegeben wird
-	 */
-	public void addEntry(String label, int returnValue) {
-		this.labels.add(label);
-		this.returnValues.add(new Integer(returnValue));
-	}
-	
-	/**
-	 * Zeigt das Menü an
-	 * @return Die Konstante des ausgewählten Menüeintrags
-	 */
-	public int show()  {
-		int selection = -1;
-		
-		BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
-		
-		while(selection == -1) {
-			System.out.println(title+":");
-			
-			for(int i = 0; i < labels.size(); ++i) {
-				System.out.println("["+(i+1)+"] "+labels.get(i));
-			}
-			
-			System.out.print("-- ");
-			try {
-				selection = Integer.parseInt(stdin.readLine());
-			} catch (NumberFormatException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			
-			if(selection < 1 || selection > returnValues.size()) {
-				System.err.println("Ungültige Eingabe!");
-				selection = -1;
-			} 
-		}
-		
-		return returnValues.get(selection-1);
-	}
+    private String title;
+    private ArrayList<String> labels = new ArrayList<String>();
+    private ArrayList<Integer> returnValues = new ArrayList<Integer>();
+
+    /**
+     * Konstruktor.
+     *
+     * @param title Titel des Menüs z.B. "Hauptmenü"
+     */
+    public Menu(String title) {
+        super();
+        this.title = title;
+    }
+
+    /**
+     * Fügt einen Menüeintrag zum Menü hinzu
+     *
+     * @param label       Name des Eintrags
+     * @param returnValue Konstante, die bei Wahl dieses Eintrags zurückgegeben wird
+     */
+    public void addEntry(String label, int returnValue) {
+        this.labels.add(label);
+        this.returnValues.add(returnValue);
+    }
+
+    /**
+     * Zeigt das Menü an
+     *
+     * @return Die Konstante des ausgewählten Menüeintrags
+     */
+    public int show() {
+        int selection = -1;
+
+        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+
+        while (selection == -1) {
+            System.out.println(title + ":");
+
+            for (int i = 0; i < labels.size(); ++i) {
+                System.out.println("[" + (i + 1) + "] " + labels.get(i));
+            }
+
+            System.out.print("-- ");
+            try {
+                selection = Integer.parseInt(stdin.readLine());
+            } catch (NumberFormatException | IOException e) {
+                e.printStackTrace();
+            }
+
+            if (selection < 1 || selection > returnValues.size()) {
+                System.err.println("Ungültige Eingabe!");
+                selection = -1;
+            }
+        }
+
+        return returnValues.get(selection - 1);
+    }
 }
