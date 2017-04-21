4dcb9e19eba2badfa5d14f8838569af66bce0d44
caleron <paddy.buchhardt@arcor.de>
2017-04-21 21:53:17 +0200

    implemented estate menu

--- Aufgabe2/src/de/dis2011/menus/EstateMenu.java
+++ Aufgabe2/src/de/dis2011/menus/EstateMenu.java
@@ -1,23 +1,30 @@
 package de.dis2011.menus;
 
+import de.dis2011.FormUtil;
 import de.dis2011.Menu;
+import de.dis2011.data.Estate;
+import de.dis2011.data.EstateAgent;
 
 public class EstateMenu {
+
+    private static EstateAgent currentAgent;
 
     /**
      * Zeigt die Wohnobjekt-Werwaltung
      */
     public static void show() {
         //Menüoptionen
-        final int LOGIN = 0;
-        final int NEW_ESTATE = 1;
-        final int EDIT_ESTATE = 2;
-        final int DELETE_ESTATE = 3;
-        final int BACK = 4;
+        final int NEW_ESTATE = 0;
+        final int EDIT_ESTATE = 1;
+        final int DELETE_ESTATE = 2;
+        final int BACK = 3;
+
+        if (!login()) {
+            return;
+        }
 
         //Maklerverwaltungsmenü
         Menu maklerMenu = new Menu("Makler-Verwaltung");
-        maklerMenu.addEntry("Login", LOGIN);
         maklerMenu.addEntry("Neues Wohnobjekt", NEW_ESTATE);
         maklerMenu.addEntry("Bearbeite Wohnobjekt", EDIT_ESTATE);
         maklerMenu.addEntry("Entferne Wohnobjekt", DELETE_ESTATE);
@@ -28,21 +35,75 @@ public class EstateMenu {
             int response = maklerMenu.show();
 
             switch (response) {
-                case LOGIN:
-                    // noch nix
-                    break;
                 case NEW_ESTATE:
-                    // noch nix
+                    createEstate();
                     break;
                 case EDIT_ESTATE:
-                    // noch nix
+                    editEstate();
                     break;
                 case DELETE_ESTATE:
-                    // noch nix
+                    deleteEstate();
                     break;
                 case BACK:
+                    //logout the agent
+                    currentAgent = null;
                     return;
             }
         }
+    }
+
+    private static boolean login() {
+        String login = FormUtil.readString("Login");
+        String password = FormUtil.readString("Passwort");
+
+        EstateAgent agent = EstateAgent.load(login);
+
+        if (agent == null) {
+            System.out.println("Makler mit dem angegebenen Login nicht gefunden.");
+            return false;
+        }
+        if (!agent.getPassword().equals(password)) {
+            System.out.println("Passwort falsch.");
+            return false;
+        }
+        currentAgent = agent;
+        return true;
+    }
+
+    private static void createEstate() {
+        Estate estate = new Estate();
+        estate.setCity(FormUtil.readString("Stadt"));
+        estate.setStreet(FormUtil.readString("Straße"));
+        estate.setStreetNumber(FormUtil.readInt("Hausnummer"));
+        estate.setPostCode(FormUtil.readInt("PLZ"));
+        estate.setSquareArea(FormUtil.readInt("Fläche in qm"));
+
+        estate.setAgent(currentAgent.getLogin());
+        estate.save();
+        System.out.println("Immobilie wurde mit ID " + estate.getId() + " erzeugt");
+    }
+
+    private static void editEstate() {
+        Estate estate = new Estate(FormUtil.readInt("ID der Immobilie"));
+        estate.setCity(FormUtil.readString("Stadt"));
+        estate.setStreet(FormUtil.readString("Straße"));
+        estate.setStreetNumber(FormUtil.readInt("Hausnummer"));
+        estate.setPostCode(FormUtil.readInt("PLZ"));
+        estate.setSquareArea(FormUtil.readInt("Fläche in qm"));
+
+        estate.setAgent(currentAgent.getLogin());
+        estate.save();
+        System.out.println("Immobilie mit ID " + estate.getId() + " wurde bearbeitet");
+    }
+
+    private static void deleteEstate() {
+        int id = FormUtil.readInt("ID der Immobilie");
+        Estate estate = Estate.load(id);
+        if (estate == null) {
+            System.out.println("Immobilie mit ID " + id + " existiert nicht.");
+            return;
+        }
+        estate.delete();
+        System.out.println("Immobilie mit ID " + id + " wurde gelöscht.");
     }
 }
